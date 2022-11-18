72f3640fc6346200b2d1f6a665af8136d77d0f74
rdmug1421 <ralmughidi@gmail.com>
2022-11-18 08:12:27 +0300

    done header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,41 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+    //menu show
+    //==validation if contant exist==
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+//menu hidden
+//==validation if contant exist
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll('.nav__link');
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+        //when click on each nav__link we remove the menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== DARK LIGHT THEME ===============*/ 
+/*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
